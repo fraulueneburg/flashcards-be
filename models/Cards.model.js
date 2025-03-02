50e96574e95aee19dfddff2e5360982a7851f98d
fraulueneburg <github@fraulueneburg.de>
2025-03-02 22:54:43 +0100

    added spaced repetition fields

--- models/Cards.model.js
+++ models/Cards.model.js
@@ -9,14 +9,18 @@ const CardsSchema = new mongoose.Schema({
 			ref: 'Tags',
 		},
 	],
-	createdAt: { type: Date, default: Date.now },
+	box: { type: Number, default: 1 },
+	reviewDate: { type: Date, default: Date.now },
+	lastReviewDate: { type: Date, default: Date.now },
+	difficult: { type: Boolean, default: false },
+	retired: { type: Boolean, default: false },
 	statistics: {
-		dueDate: { type: Date, default: Date.now },
 		guesses: {
 			total: { type: Number, default: 0 },
 			correct: { type: Number, default: 0 },
 		},
 	},
+	createdAt: { type: Date, default: Date.now },
 })
 
 module.exports = mongoose.model('Cards', CardsSchema)
